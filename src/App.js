0decb1f257893d8ba13b480430504d0ebc93b1dd
RAHUL <rahulmeetawa@gmail.com>
2023-06-22 15:45:05 +0530

    render Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 };

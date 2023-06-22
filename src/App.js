3b67ab9966e432acbe6c5a7002a222d408e3b587
RAHUL <rahulmeetawa@gmail.com>
2023-06-22 15:39:47 +0530

    render Content component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Content from "./components/Content";
 
 const App = () => {
   return (
-    <div className="header-styles">
+    <div className="app">
       <Header />
+      <Content />
     </div>
   );
 };

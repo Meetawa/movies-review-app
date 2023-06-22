11b326b7d0959f8a53e9a2000ac3bcd4fc4e2446
RAHUL <rahulmeetawa@gmail.com>
2023-06-22 16:36:06 +0530

    add Content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Content = () => {
-  return <div></div>;
+const Content = (props) => {
+  console.log(props.title);
+  return (
+    <div className="content">
+      <img src="" alt="no Image" />
+      <p>{props.title}</p>
+      <p>{props.Year}</p>
+    </div>
+  );
 };
 
 export default Content;

25464db332418b85feeb51df97dbc48156bdc509
RAHUL <rahulmeetawa@gmail.com>
2023-06-22 16:13:52 +0530

    render Data component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import Data from "./Data.json";
 
 const App = () => {
+  console.log(Data);
   return (
     <div className="app">
       <Header />
-      <Content />
+      {Data.map((Element) => {
+        return <Content title={Element.Title} Year={Element.Year} />;
+      })}
+
       <Footer />
     </div>
   );

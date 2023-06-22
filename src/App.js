abc31f459ac5038d5eaf762a1310eb6814fe7041
RAHUL <rahulmeetawa@gmail.com>
2023-06-22 15:38:17 +0530

    fix some  issues

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
+    <div className="header-styles">
       <Header />
-    </>
+    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <h1>Movies Review</h1>
     </div>
   );
